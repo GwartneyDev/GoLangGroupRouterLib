374a3267dbe23387754aeb21b55e66961cf7597b
Gwartneydev <gwartneydev@gmail.com>
2024-04-21 23:03:06 -0700

    adding starter boilerplate code

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "context"
+
+type HandlerFunc func(context.Context)
+
+type HandlersChain []HandlerFunc
+
+type RoutesGroup struct {
+	Handlers HandlersChain
+	basePath string
+}
+
 func main() {
 
 }
